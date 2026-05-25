eb7a2b1a7a44c3c2fc77f75f45cea261a76872bf
suzuki787 <mercedesbenzagm@gmail.com>
2026-05-25 12:30:05 +0500

    Update ​vending.js

--- ​vending.js
+++ ​vending.js
@@ -1,1 +1,17 @@
-1
+// Savdo avtomati mantig'i
+const VendingMachine = {
+    balance: 0,
+    
+    addMoney: function(amount) {
+        this.balance += amount;
+        console.log("Balans yangilandi: " + this.balance);
+    },
+
+    buyCar: function(price) {
+        if (this.balance >= price) {
+            this.balance -= price;
+            return true;
+        }
+        return false;
+    }
+};
